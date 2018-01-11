8edfdefd6c9f247203ce4185f84320df63833ed4
Israel Hofman <icehofman@hotmail.com>
2018-01-11 14:14:49 -0200

    Fixed user permission

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,20 @@
-FROM jenkins/jenkins:lts-alpine
- 
+FROM jenkins/jenkins:lts
+
 ENV JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
+ENV JENKINS_USER admin
+ENV JENKINS_PASS admin
+
+USER root
+
+RUN groupmod -g 1000 jenkins
+RUN usermod -u 1000 -g 1000 jenkins
+RUN chown -R 1000:1000 /var/jenkins_home
+
+USER jenkins
  
 COPY security.groovy /usr/share/jenkins/ref/init.groovy.d/security.groovy
  
 COPY plugins.txt /usr/share/jenkins/ref/plugins.txt
 RUN /usr/local/bin/install-plugins.sh < /usr/share/jenkins/ref/plugins.txt
+
+VOLUME /var/jenkins_home
--- security.groovy
+++ security.groovy
@@ -2,16 +2,15 @@
  
 import jenkins.model.*
 import hudson.security.*
-import jenkins.security.s2m.AdminWhitelistRule
- 
-def instance = Jenkins.getInstance()
- 
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("admin", "admin")
-instance.setSecurityRealm(hudsonRealm)
- 
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-instance.setAuthorizationStrategy(strategy)
-instance.save()
- 
-Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
+
+def env = System.getenv()
+
+def jenkins = Jenkins.getInstance()
+jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
+jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
+
+def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
+user.save()
+
+jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+jenkins.save()
